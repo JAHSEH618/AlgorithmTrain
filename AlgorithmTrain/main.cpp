233692fd31164a00c0e5d8f68efd59402c1bd35b
Kaiyuan Liu <jahse_hliu@163.com>
2024-12-12 00:05:26 +0800

    Initial Commit

---
+++ AlgorithmTrain/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AlgorithmTrain
+//
+//  Created by Kaiyuan Liu on 2024/12/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
