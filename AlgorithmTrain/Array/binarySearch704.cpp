b1aa0c830739b910e98750d9ab45d887417115b5
Kaiyuan Liu <jahse_hliu@163.com>
2024-12-12 00:05:26 +0800

    first commit: test

---
+++ AlgorithmTrain/Array/binarySearch704.cpp
@@ -0,0 +1,20 @@
+//
+//  binarySearch704.cpp
+//  AlgorithmTrain
+//
+//  Created by Kaiyuan Liu on 2024/12/12.
+//
+
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int pos = 0;
+        int left = 0;
+        int right = nums.size() - 1;
+        return pos;
+    };
+};
