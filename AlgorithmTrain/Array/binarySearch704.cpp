4e313d5457456a08a951de1313e946d260235583
Kaiyuan Liu <jahse_hliu@163.com>
2024-12-19 01:15:18 +0800

    binarySearch

--- AlgorithmTrain/Array/binarySearch704.cpp
+++ AlgorithmTrain/Array/binarySearch704.cpp
@@ -5,16 +5,34 @@
 //  Created by Kaiyuan Liu on 2024/12/12.
 //
 
-#include <stdio.h>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int binarySearch(vector<int>& nums, int target) {
         int pos = 0;
         int left = 0;
-        int right = nums.size() - 1;
-        return pos;
+        int right = (int) nums.size() - 1;
+        while (left <= right) {
+            pos = left + ((right - left) / 2);
+            if (nums[pos] > target) {
+                right = pos - 1;
+            } else if (nums[pos] < target) {
+                left = pos + 1;
+            } else {
+                return pos;
+            }
+        }
+        return -1;
     };
 };
+
+//int main() {
+//    vector<int> v1 = {1, 2, 5, 6, 9, 12, 24};
+//    int target = 9;
+//    Solution sol;
+//    cout << sol.binarySearch(v1, target) << endl;
+//    return 0;
+//};
