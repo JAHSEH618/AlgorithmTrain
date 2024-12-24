956be3bc5de880d72f5070636b26528ca6286885
Kaiyuan Liu <jahse_hliu@163.com>
2024-12-24 23:20:28 +0800

    TODO: Redo 34 searchRange

--- AlgorithmTrain/Array/searchRange34.cpp
+++ AlgorithmTrain/Array/searchRange34.cpp
@@ -13,16 +13,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res = {-1, -1};
         int left = leftBorder(nums, target);
         int right = rightBorder(nums, target);
         if (left == -2 || right == -2) {
-            res = {-1, -1};
+            return {-1, -1};
         }
         if (right - left > 1) {
-            res = {left + 1, right - 1};
+            return {left + 1, right - 1};
         }
-        return res;
+        return {-1, -1};
     }
     
 private:
@@ -46,16 +45,16 @@ private:
 private:
     int leftBorder(vector<int>& nums, int target) {
         int leftBorder = -2;
-        int mid;
         int left = 0;
-        int right =(int) nums.size() - 1;
-        while (left <= right) {
-            mid = left + ((right - left) / 2);
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
+        int right = (int) nums.size() - 1;
+        int mid;
+        while(left <= right) {
+            mid = left + ((right- left) / 2);
+            if (nums[mid] >= target) {
                 right = mid - 1;
                 leftBorder = right;
+            } else {
+                left = mid + 1;
             }
         }
         return leftBorder;
@@ -63,8 +62,8 @@ private:
 };
 
 int main() {
-    vector<int> nums = {1, 2, 2, 2, 3, 5};
-    int target = 2;
+    vector<int> nums = {2, 2};
+    int target = 3;
     Solution s;
     cout << s.searchRange(nums, target)[0] << "\t" << s.searchRange(nums, target)[1] << endl;
     return 0;
